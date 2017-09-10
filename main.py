c7dac44097a11ed1a1d0a95c1e1d08908a381de2
Yan Zhan <yanzhan91@gmail.com>
2017-09-10 00:29:11 -0500

    Add preset verification and preset intent

--- main.py
+++ main.py
@@ -44,6 +44,7 @@ def check_bus(bus_id, stop_id):
 
 @ask.intent('SetBusIntent')
 def set_bus(bus_id, stop_id, preset):
+    preset = check_preset_syntax(preset)
     logger.info('session = %s' % session_id)
     logger.info('%s: Setting Bus %s at %s for %s...' % (session_id, bus_id, stop_id, preset))
     try:
@@ -57,10 +58,9 @@ def set_bus(bus_id, stop_id, preset):
     return statement(set_bus_success_message).simple_card('Set Bus Status', remove_html(set_bus_success_message))
 
 
-@ask.intent('GetBusPresetIntent')
-def get_bus_preset(preset):
-    if not preset:
-        preset = 'preset 1'
+@ask.intent('GetBusIntent')
+def get_bus(preset):
+    preset = check_preset_syntax(preset)
     logger.info('session = %s' % session_id)
     logger.info('%s: Getting Bus at %s...' % (session_id, preset))
     try:
@@ -71,6 +71,25 @@ def get_bus_preset(preset):
         logger.error(e)
         internal_error_message = render_template('internal_error_message')
         return statement(internal_error_message).simple_card('Get Bus Status', internal_error_message)
+
+
+@ask.intent('GetPresetIntent')
+def get_preset(preset):
+    preset = check_preset_syntax(preset)
+    bus_id, stop_id = GetBusIntent.get_bus(context.System.user.userId, preset)
+    get_preset_message = render_template('get_preset_message', preset=preset, bus_id=bus_id, stop_id=stop_id)
+    return statement(get_preset_message).simple_card(preset.title(), remove_html(get_preset_message))
+
+
+def check_preset_syntax(preset):
+    if not preset and not re.compile('preset\\s[0-9]+').match(preset):
+        return 'preset 1'
+    elif preset == 'preset to':
+        return 'preset 2'
+    elif preset == 'preset too':
+        return 'preset 2'
+    else:
+        return preset
 
 
 def remove_html(text, return_char=True):
