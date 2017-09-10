67849763fc9384626be7a0e1b448f58ff58741a9
Yan Zhan <yanzhan91@gmail.com>
2017-09-09 23:27:25 -0500

    Add simple cards to responses

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import logging
 from flask import Flask, render_template
 from flask_ask import Ask, statement, question, context
 import uuid
+import re
 import CheckBusIntent
 import SetBusIntent
 import GetBusIntent
@@ -15,12 +16,16 @@ session_id = uuid.uuid4()
 
 @ask.launch
 def launch():
-    return question(render_template('welcome')).reprompt(render_template('help'))
+    welcome_text = render_template('welcome')
+    return question(welcome_text)\
+        .simple_card('Welcome to AustinTransit', remove_html(welcome_text))\
+        .reprompt(render_template('help'))
 
 
 @ask.intent('AMAZON.HelpIntent')
 def help():
-    return question(render_template('help'))
+    help_text = render_template('help')
+    return question(help_text).simple_card('AustinTransit Help', remove_html(help_text, False))
 
 
 @ask.intent('CheckBusIntent', convert={'bus_id': int, 'stop_id': int})
@@ -30,9 +35,11 @@ def check_bus(bus_id, stop_id):
     minutes = CheckBusIntent.check_bus(bus_id, stop_id)
     logging.info('%s: Minutes received: %s' % (session_id, minutes))
     if len(minutes) == 0:
-        return statement(render_template('no_bus_message', bus_id=bus_id, stop_id=stop_id))
+        no_bus_message = render_template('no_bus_message', bus_id=bus_id, stop_id=stop_id)
+        return statement(no_bus_message).simple_card('Check Bus Status', remove_html(no_bus_message))
     minutes_string = ' and '.join(str(x) for x in minutes)
-    return statement(render_template('bus_minutes_message', bus_id=bus_id, stop_id=stop_id, minutes=minutes_string))
+    bus_minutes_message = render_template('bus_minutes_message', bus_id=bus_id, stop_id=stop_id, minutes=minutes_string)
+    return statement(bus_minutes_message).simple_card('Check Bus Status', remove_html(bus_minutes_message))
 
 
 @ask.intent('SetBusIntent')
@@ -43,19 +50,32 @@ def set_bus(bus_id, stop_id, preset):
         SetBusIntent.set_bus(context.System.user.userId, bus_id, stop_id, preset)
     except Exception as e:
         logger.error(e)
-        return statement(render_template('internal_error_message'))
+        internal_error_message = render_template('internal_error_message')
+        return statement(internal_error_message).simple_card('Set Bus Status', internal_error_message)
     logger.info('%s: Set bus %s at %s was successful' % (session_id, bus_id, stop_id))
-    return statement(render_template('set_bus_success_message', bus_id=bus_id, stop_id=stop_id, preset=preset))
+    set_bus_success_message = render_template('set_bus_success_message', bus_id=bus_id, stop_id=stop_id, preset=preset)
+    return statement(set_bus_success_message).simple_card('Set Bus Status', remove_html(set_bus_success_message))
 
 
-@ask.intent('GetBusIntent')
-def get_bus(preset):
+@ask.intent('GetBusPresetIntent')
+def get_bus_preset(preset):
+    if not preset:
+        preset = 'preset 1'
     logger.info('session = %s' % session_id)
-    logger.info('%s: Getting Bus at %s...' % preset)
+    logger.info('%s: Getting Bus at %s...' % (session_id, preset))
     try:
         bus_id, stop_id = GetBusIntent.get_bus(context.System.user.userId, preset)
         logger.info('%s: Bus retrieved was %s at %s' % (session_id, bus_id, stop_id))
         return check_bus(bus_id, stop_id)
     except Exception as e:
         logger.error(e)
-        return statement(render_template('internal_error_message'))
+        internal_error_message = render_template('internal_error_message')
+        return statement(internal_error_message).simple_card('Get Bus Status', internal_error_message)
+
+
+def remove_html(text, return_char=True):
+    if return_char:
+        regex = '<[^<]*?>|\\n'
+    else:
+        regex = '<[^<]*?>'
+    return re.sub(regex, '', text)
